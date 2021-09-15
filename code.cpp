2ea0d0de30ade8955bd990b41fbdd25bbca4d082
roshni2072004 <65917090+roshni2072004@users.noreply.github.com>
2021-09-15 16:13:29 +0530

    code.cpp added

---
+++ code.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+void menu()
+{
+    cout << "**********Menu***********"<<endl;
+    cout << "1. Check Amount" << endl;
+    cout << "2. Deposit Money" << endl;
+    cout << "3. Withdraw Money" << endl;
+    cout << "**********Menu***********"<<endl;
+}
+int main()
+{
+    
+    double amount = 500;
+    int choice;
+    do
+    {
+        menu();
+        cout<<"Choice:";
+        cin>>choice;
+        // system("cls");
+        switch (choice)
+        {
+            case 1:
+                cout << "Total Balance :"
+                    << "Rs." << amount << endl;
+                    break;
+            case 2:
+                double Depositamt;
+                cout<< "Enter the amount to Deposit: ";
+                cin>> Depositamt;
+                amount += Depositamt;
+                break;
+            case 3:
+                double withrawamt;
+                cout << "Enter the amount to Withdraw: ";
+                cin >> withrawamt;
+                if(withrawamt<= amount){
+                    amount -= withrawamt;
+                }
+                else
+                {
+                    cout << "Not enough Money"<<endl;
+                }
+                break;
+
+            case 4:
+                exit;
+        }
+    } while (choice != 4);
+
+    //system("pause>0");
+    return 0;
+}
